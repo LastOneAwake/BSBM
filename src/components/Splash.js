a52ea508938cfd899b30e6cda90e95684e4b2fbc
Ruben Melendez <ruben@MacBook-Pro.lan1>
2021-05-01 18:10:17 -0700

    assets added, animated gradient bg

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './splash.scss';
+import logo from '../assets/headerLogo.png';
 
-export default function Splash(){
- 
-    return(
-        <div id='splash' className='fullPageSection'>
-            <div id='splashShape' className='bgShape' > 
-            {/* <div id='splashShape' className='bgShape circle' />  */}
+export default function Splash() {
+
+    return (
+        <div id='splash' className='fullPageSection centeredFlex'>
+            <div id='splashGradientBG'/>
+            <div id='splashShape' className='bgShape'>
+                <img src={logo} alt='Body Sculpting By Mito logo' />
             </div>
-
         </div>
 
     )
-    
+
 }
