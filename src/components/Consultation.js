b44b816e6d6e4fdf0a2931216672bd443b43b20a
Ruben Melendez <ruben@MacBook-Pro.lan1>
2021-05-03 15:40:20 -0700

    styled form

--- src/components/Consultation.js
+++ src/components/Consultation.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './consultation.scss';
 
 export default function Consultation() {
+    const [emailVal, updateEmailVal] = useState('');
+    const [naemVal, updateNameVal] = useState('');
+    const [numberVal, updateNumberVal] = useState('');
 
     return (
         <div id='consultation' className='fullPageSection centeredFlex'>
@@ -9,7 +12,20 @@ export default function Consultation() {
                 <div id='consultationHeader' className='sectionHeader flex1'>Schedule a free consultation</div>
                 <div id='consultationImg' className='flex1'></div>
             </div>
-            <div id='formholder' className='flex1'>
+            <div id='formholder' className='flex1 centeredFlex'>
+
+                <form className='centeredFlex'>
+                    <span id='serviceSelectionHeader'>Service you're intrested in?</span>
+                    <div id='serviceHolder' className='centeredFlex'>
+                        <div className='formService'>HIFU/Ultherapy</div>
+                        <div className='formService'>EMSculpt</div>
+                        <div className='formService'>General Inqury</div>
+                    </div>
+                    <div className='formInput centeredFlex'><label>Name</label><input type='text'></input></div>
+                    <div className='formInput centeredFlex'><label>PhoneNumber</label><input type='text'></input></div>
+                    <div className='formInput centeredFlex'><label>Email</label><input type='number'></input></div>
+                    <button >Send</button>
+                </form>
             </div>
         </div>
 
