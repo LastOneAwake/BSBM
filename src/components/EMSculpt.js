7870780458cd88181fe44de92d5373020a112f8d
Ruben Melendez <ruben@MacBook-Pro.lan1>
2021-05-03 21:04:15 -0700

    emsculpt styled

--- src/components/EMSculpt.js
+++ src/components/EMSculpt.js
@@ -8,24 +8,27 @@ export default function EMSculpt() {
 
     return (
         <div id='emsculpt' className='fullPageSection centeredFlex'>
-            <div id='emsculptHeader' className='centeredFlex'>
-                <div id='emsculptShape' className='centeredFlex'>
+            <div id='emsculptHeader' className='centeredFlex emsSection'>
                 What is EMSculpt?
-                </div>
             </div>
-            <div id='emsculptText' className='flex1 centeredFlex'>
-                <p>is the first device to build muscle and sculpt your body. 
-                    Through the high intensity electromagnetic therapy procedure one can enlarge current muscles, as well as grow new muscle fibers. 
+
+            <div id='emsculptText' className='flex1 centeredFlex emsSection'>
+                <p id='desc'>EMSculpt is the first device to build muscle and sculpt your body.
+                    <br />
+                    Through the high intensity electromagnetic therapy procedure one can enlarge current muscles, as well as grow new muscle fibers.
                     The Emsculpt procedure is FDA cleared to treat your abdominals, buttocks, arms, calves and thighs. A great non-surgical alternative to the brazilian butt lift.
-                    <br/>
+                    <br />
+                    <br />
                     A single EMSCULPT session causes thousands of powerful muscle contractions which are extremely important in improving the tone and strength of your muscles.
-                    <br/>
-                    These powerful induced muscle contractions not achievable through voluntary contractions. The muscle tissue is forced to adapt to such extreme condition. 
-                    It responds with a deep remodeling of its inner structure that results in muscle building and sculpting your body. 
-                    </p>
-                    <p>
-                    30 minutes treatment = 20,000 times sit ups.                     
-                    </p>
+                    <br />
+                    <br />
+                    These powerful induced muscle contractions not achievable through voluntary contractions. The muscle tissue is forced to adapt to such extreme condition.
+                    <br />
+                    It responds with a deep remodeling of its inner structure that results in muscle building and sculpting your body.
+                </p>
+                <p id='situps'>
+                    30 minutes treatment = 20,000 times sit ups.
+                  </p>
             </div>
 
         </div>
