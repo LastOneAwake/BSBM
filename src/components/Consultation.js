24abcf647705954d67dbe27e50359900d93b0a83
Ruben Melendez <ruben@MacBook-Pro.lan1>
2021-05-03 22:03:46 -0700

    form state handling + butts

--- src/components/Consultation.js
+++ src/components/Consultation.js
@@ -5,6 +5,11 @@ export default function Consultation() {
     const [emailVal, updateEmailVal] = useState('');
     const [nameVal, updateNameVal] = useState('');
     const [numberVal, updateNumberVal] = useState('');
+    const [hifuSelected, updateHifuSelected] = useState(false);
+    const [emsSelected, updateEMSSelected] = useState(false);
+    const [genSelected, updateGenSelected] = useState(false);
+
+
 
     return (
         <div id='consultation' className='fullPageSection centeredFlex'>
@@ -17,9 +22,24 @@ export default function Consultation() {
                 <form className='centeredFlex'>
                     <span id='serviceSelectionHeader'>Service you're intrested in?</span>
                     <div id='serviceHolder' className='centeredFlex'>
-                        <div className='formService'>HIFU/Ultherapy</div>
-                        <div className='formService'>EMSculpt</div>
-                        <div className='formService'>General Inqury</div>
+                        <div 
+                        className={'formService' + (hifuSelected ? ' selected' : '')}
+                        onClick={()=>{
+                            updateHifuSelected(!hifuSelected);
+                        }}
+                        >HIFU/Ultherapy</div>
+                        <div 
+                        className={'formService' + (emsSelected ? ' selected' : '')}
+                        onClick={()=>{
+                            updateEMSSelected(!emsSelected);
+                        }}
+                        >EMSculpt</div>
+                        <div 
+                        className={'formService' + (genSelected ? ' selected' : '')}
+                        onClick={()=>{
+                            updateGenSelected(!genSelected);
+                        }}
+                        >General Inqury</div>
                     </div>
                     <div className='formInput centeredFlex'>
                         <label>Name</label>
