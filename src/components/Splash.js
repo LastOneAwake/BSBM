347fe89dd9c26ae7e8a88eb8607f90e2d996e94a
Ruben Melendez <ruben@MacBook-Pro.local>
2021-06-24 20:10:39 -0700

    splash edits

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -13,11 +13,7 @@ export default function Splash() {
             <p id='introBlock'>
                 We specialize in non-surgical, FDA
                 approved treatments for permanent fat
-                reduction. 
-                {/* These treatments involve killing and/or liquifying visible fat cells via freezing, laser, or high frequency sound waves. */}
-                {/* The body's lymphatic system then kicks into gear to remove these broken-down fat cells, giving you a dramatically slimmer appearance.  */}
-                Our goal is to provide our
-                clients with the best results.
+                reduction. Our goal is to provide our clients with the best results.
             </p>
         </div>
 
